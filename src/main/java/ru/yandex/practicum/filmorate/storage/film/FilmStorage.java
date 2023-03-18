a8694cbb65b9804d2902297be77aa1d22c2da402
VarvaraMatveeva <forestknight567@outlook.com>
2023-03-18 19:55:24 +0300

    refactor: изменение реализации storage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,14 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
-import java.util.Collection;
-import java.util.HashMap;
+import ru.yandex.practicum.filmorate.storage.Storage;
 
-public interface FilmStorage {
-    Film addFilm(Film film);
-    Film updateFilm(Film film);
-    Film getFilmById(Long id);
-    void deleteFilm(Long id);
-    Collection<Film> getAllFilms();
-    HashMap<Long, Film> getFilmHashMap();
+public interface FilmStorage extends Storage<Film> {
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,14 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
-import java.util.Collection;
-import java.util.HashMap;
+import ru.yandex.practicum.filmorate.storage.Storage;
 
-public interface UserStorage {
-    User createUser(User user);
-    User updateUser(User user);
-    void deleteUser(Long id);
-    Collection<User> getAllUsers();
-    User getUserById(Long id);
-    HashMap<Long, User> getUserHashMap();
+public interface UserStorage extends Storage<User> {
 }
