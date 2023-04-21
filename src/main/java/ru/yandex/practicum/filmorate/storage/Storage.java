c2d03a23aabb4fd8db6d37b8351ac4b21e78e967
VarvaraMatveeva <forestknight567@outlook.com>
2023-04-21 15:57:38 +0300

    style: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
@@ -2,11 +2,15 @@ package ru.yandex.practicum.filmorate.storage;
 
 import java.util.Collection;
 import java.util.Optional;
-
 public interface Storage<T> {
+
     T add(T obj);
+
     Optional<T> update(T obj);
+
     Optional<T> getById(Long id);
+
     void delete(Long id);
+
     Collection<T> getAll();
 }
